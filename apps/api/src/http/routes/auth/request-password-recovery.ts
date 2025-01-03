efd77bd5090d1673d25338af7bac57d03f9882b4
Eduardo Nogueira <eduardodudunog@gmail.com>
2025-01-03 16:25:12 -0300

    feat(backend): add reset password route

---
+++ apps/api/src/http/routes/auth/request-password-recovery.ts
@@ -0,0 +1,48 @@
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { prisma } from '@/lib/prisma'
+
+export async function requestPasswordRecovery(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/password/recovery',
+    {
+      schema: {
+        tags: ['auth'],
+        summary: 'Get authenticated user profile',
+        body: z.object({
+          email: z.string().email(),
+        }),
+        response: {
+          201: z.null(),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { email } = request.body
+
+      const userFromEmail = await prisma.user.findUnique({
+        where: { email },
+      })
+
+      if (!userFromEmail) {
+        // We don't want people to know if user really exists
+        return reply.status(201).send()
+      }
+
+      const { id: code } = await prisma.token.create({
+        data: {
+          type: 'PASSWORD_RECOVER',
+          userId: userFromEmail.id,
+        },
+      })
+
+      // Send e-mail with password recovery link
+
+      console.log('Recovery password token', code)
+
+      return reply.status(201).send()
+    },
+  )
+}
