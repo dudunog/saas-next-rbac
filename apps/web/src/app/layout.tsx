dbdc9d0a4ac124e88bf2af277219ed52eed75f0b
Eduardo Nogueira <eduardodudunog@gmail.com>
2025-01-08 14:59:08 -0300

    feat(frontend): front-end setup

---
+++ apps/web/src/app/layout.tsx
@@ -0,0 +1,19 @@
+import './globals.css'
+
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Create Next App',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
